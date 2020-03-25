8d15fcebc115f2e54c015f197c8f29c3d329ec2a
Pietro Lorefice <pietro.lorefice@gmail.com>
2020-03-25 22:37:26 +0100

    set1: add solution to 'Detect AES in ECB mode'

--- src/sets/set1.rs
+++ src/sets/set1.rs
@@ -143,6 +143,21 @@ pub fn aes_in_ecb_mode<I: AsRef<[u8]>>(input: I) -> Result<String> {
     )?)?)
 }
 
+/// Set 1 - Challenge 8
+/// Detect AES in ECB mode
+pub fn detect_aes_in_ecb_mode<I: AsRef<[u8]>>(input: I) -> Result<String> {
+    for line in input.as_ref().split(|&b| b == b'\n') {
+        let line = hex::decode(line)?;
+
+        for pair in line.chunks(16).combinations(2) {
+            if pair[0] == pair[1] {
+                return Ok(hex::encode(line));
+            }
+        }
+    }
+    Err("Ciphertext not detected!".into())
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -222,5 +237,16 @@ mod tests {
                 .unwrap()
                 .contains("Play that funky music")
         );
+    }
+
+    #[test]
+    fn run_detect_aes_in_ecb_mode() {
+        assert_eq!(
+            detect_aes_in_ecb_mode(&include_bytes!("../../data/set1/8.txt")[..]).unwrap(),
+            "d880619740a8a19b7840a8a31c810a3d08649af70dc06f4fd5d2d69c744cd283e2dd052f6b641dbf\
+             9d11b0348542bb5708649af70dc06f4fd5d2d69c744cd2839475c9dfdbc1d46597949d9c7e82bf5a\
+             08649af70dc06f4fd5d2d69c744cd28397a93eab8d6aecd566489154789a6b0308649af70dc06f4f\
+             d5d2d69c744cd283d403180c98c8f6db1f2a3f9c4040deb0ab51b29933f2c123c58386b06fba186a"
+        )
     }
 }
